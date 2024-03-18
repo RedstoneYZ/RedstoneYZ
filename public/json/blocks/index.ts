d7211e6834ea13e108c1062f0445f94f0b4a24ba
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-03-18 17:23:08 +0800

    chore: add assets

---
+++ public/json/blocks/index.ts
@@ -0,0 +1,132 @@
+import { parseTexture } from "../../../components/Canvas/core/utils";
+import _block from "./block.json";
+import _comparator_on_subtract from "./comparator_on_subtract.json";
+import _comparator_on from "./comparator_on.json";
+import _comparator_subtract from "./comparator_subtract.json";
+import _comparator from "./comparator.json";
+import _cube from "./cube.json";
+import _cube_all from "./cube_all.json";
+import _cube_column from "./cube_column.json";
+import _glass from "./glass.json";
+import _iron_block from "./iron_block.json";
+import _lever_on from "./lever_on.json";
+import _lever from "./lever.json";
+import _redstone_dust_dot from "./redstone_dust_dot.json";
+import _redstone_dust_side_alt from "./redstone_dust_side_alt.json";
+import _redstone_dust_side_alt0 from "./redstone_dust_side_alt0.json";
+import _redstone_dust_side_alt1 from "./redstone_dust_side_alt1.json";
+import _redstone_dust_side from "./redstone_dust_side.json";
+import _redstone_dust_side0 from "./redstone_dust_side0.json";
+import _redstone_dust_side1 from "./redstone_dust_side1.json";
+import _redstone_dust_up from "./redstone_dust_up.json";
+import _redstone_lamp from "./redstone_lamp.json";
+import _redstone_lamp_on from "./redstone_lamp_on.json";
+import _redstone_torch_off from "./redstone_torch_off.json";
+import _redstone_torch from "./redstone_torch.json";
+import _redstone_wall_torch_off from "./redstone_wall_torch_off.json";
+import _redstone_wall_torch from "./redstone_wall_torch.json";
+import _repeater_1tick_locked from "./repeater_1tick_locked.json";
+import _repeater_1tick_on_locked from "./repeater_1tick_on_locked.json";
+import _repeater_1tick_on from "./repeater_1tick_on.json";
+import _repeater_1tick from "./repeater_1tick.json";
+import _repeater_2tick_locked from "./repeater_2tick_locked.json";
+import _repeater_2tick_on_locked from "./repeater_2tick_on_locked.json";
+import _repeater_2tick_on from "./repeater_2tick_on.json";
+import _repeater_2tick from "./repeater_2tick.json";
+import _repeater_3tick_locked from "./repeater_3tick_locked.json";
+import _repeater_3tick_on_locked from "./repeater_3tick_on_locked.json";
+import _repeater_3tick_on from "./repeater_3tick_on.json";
+import _repeater_3tick from "./repeater_3tick.json";
+import _repeater_4tick_locked from "./repeater_4tick_locked.json";
+import _repeater_4tick_on_locked from "./repeater_4tick_on_locked.json";
+import _repeater_4tick_on from "./repeater_4tick_on.json";
+import _repeater_4tick from "./repeater_4tick.json";
+import _target from "./target.json";
+import _template_torch_wall from "./template_torch_wall.json";
+import _template_torch from "./template_torch.json";
+
+const _blockData = {
+  block: _block,
+  comparator_on_subtract: _comparator_on_subtract, 
+  comparator_on: _comparator_on, 
+  comparator_subtract: _comparator_subtract, 
+  comparator: _comparator, 
+  cube: _cube, 
+  cube_all: _cube_all, 
+  cube_column: _cube_column, 
+  glass: _glass, 
+  iron_block: _iron_block, 
+  lever_on: _lever_on, 
+  lever: _lever, 
+  redstone_dust_dot: _redstone_dust_dot, 
+  redstone_dust_side_alt: _redstone_dust_side_alt, 
+  redstone_dust_side_alt0: _redstone_dust_side_alt0, 
+  redstone_dust_side_alt1: _redstone_dust_side_alt1, 
+  redstone_dust_side: _redstone_dust_side, 
+  redstone_dust_side0: _redstone_dust_side0, 
+  redstone_dust_side1: _redstone_dust_side1, 
+  redstone_dust_up: _redstone_dust_up, 
+  redstone_lamp: _redstone_lamp, 
+  redstone_lamp_on: _redstone_lamp_on, 
+  redstone_torch_off: _redstone_torch_off, 
+  redstone_torch: _redstone_torch, 
+  redstone_wall_torch_off: _redstone_wall_torch_off, 
+  redstone_wall_torch: _redstone_wall_torch, 
+  repeater_1tick_locked: _repeater_1tick_locked, 
+  repeater_1tick_on_locked: _repeater_1tick_on_locked, 
+  repeater_1tick_on: _repeater_1tick_on, 
+  repeater_1tick: _repeater_1tick, 
+  repeater_2tick_locked: _repeater_2tick_locked, 
+  repeater_2tick_on_locked: _repeater_2tick_on_locked, 
+  repeater_2tick_on: _repeater_2tick_on, 
+  repeater_2tick: _repeater_2tick, 
+  repeater_3tick_locked: _repeater_3tick_locked, 
+  repeater_3tick_on_locked: _repeater_3tick_on_locked, 
+  repeater_3tick_on: _repeater_3tick_on, 
+  repeater_3tick: _repeater_3tick, 
+  repeater_4tick_locked: _repeater_4tick_locked, 
+  repeater_4tick_on_locked: _repeater_4tick_on_locked, 
+  repeater_4tick_on: _repeater_4tick_on, 
+  repeater_4tick: _repeater_4tick, 
+  target: _target, 
+  template_torch_wall: _template_torch_wall, 
+  template_torch: _template_torch
+};
+
+export const comparator_on_subtract   = parseTexture(_blockData, _blockData.comparator_on_subtract);
+export const comparator_on            = parseTexture(_blockData, _blockData.comparator_on);
+export const comparator_subtract      = parseTexture(_blockData, _blockData.comparator_subtract);
+export const comparator               = parseTexture(_blockData, _blockData.comparator);
+export const glass                    = parseTexture(_blockData, _blockData.glass);
+export const iron_block               = parseTexture(_blockData, _blockData.iron_block);
+export const lever_on                 = parseTexture(_blockData, _blockData.lever_on);
+export const lever                    = parseTexture(_blockData, _blockData.lever);
+export const redstone_dust_dot        = parseTexture(_blockData, _blockData.redstone_dust_dot);
+export const redstone_dust_side_alt0  = parseTexture(_blockData, _blockData.redstone_dust_side_alt0);
+export const redstone_dust_side_alt1  = parseTexture(_blockData, _blockData.redstone_dust_side_alt1);
+export const redstone_dust_side0      = parseTexture(_blockData, _blockData.redstone_dust_side0);
+export const redstone_dust_side1      = parseTexture(_blockData, _blockData.redstone_dust_side1);
+export const redstone_dust_up         = parseTexture(_blockData, _blockData.redstone_dust_up);
+export const redstone_lamp_on         = parseTexture(_blockData, _blockData.redstone_lamp_on);
+export const redstone_lamp            = parseTexture(_blockData, _blockData.redstone_lamp);
+export const redstone_torch_off       = parseTexture(_blockData, _blockData.redstone_torch_off);
+export const redstone_torch           = parseTexture(_blockData, _blockData.redstone_torch);
+export const redstone_wall_torch_off  = parseTexture(_blockData, _blockData.redstone_wall_torch_off);
+export const redstone_wall_torch      = parseTexture(_blockData, _blockData.redstone_wall_torch);
+export const repeater_1tick_locked    = parseTexture(_blockData, _blockData.repeater_1tick_locked);
+export const repeater_1tick_on_locked = parseTexture(_blockData, _blockData.repeater_1tick_on_locked);
+export const repeater_1tick_on        = parseTexture(_blockData, _blockData.repeater_1tick_on);
+export const repeater_1tick           = parseTexture(_blockData, _blockData.repeater_1tick);
+export const repeater_2tick_locked    = parseTexture(_blockData, _blockData.repeater_2tick_locked);
+export const repeater_2tick_on_locked = parseTexture(_blockData, _blockData.repeater_2tick_on_locked);
+export const repeater_2tick_on        = parseTexture(_blockData, _blockData.repeater_2tick_on);
+export const repeater_2tick           = parseTexture(_blockData, _blockData.repeater_2tick);
+export const repeater_3tick_locked    = parseTexture(_blockData, _blockData.repeater_3tick_locked);
+export const repeater_3tick_on_locked = parseTexture(_blockData, _blockData.repeater_3tick_on_locked);
+export const repeater_3tick_on        = parseTexture(_blockData, _blockData.repeater_3tick_on);
+export const repeater_3tick           = parseTexture(_blockData, _blockData.repeater_3tick);
+export const repeater_4tick_locked    = parseTexture(_blockData, _blockData.repeater_4tick_locked);
+export const repeater_4tick_on_locked = parseTexture(_blockData, _blockData.repeater_4tick_on_locked);
+export const repeater_4tick_on        = parseTexture(_blockData, _blockData.repeater_4tick_on);
+export const repeater_4tick           = parseTexture(_blockData, _blockData.repeater_4tick);
+export const target                   = parseTexture(_blockData, _blockData.target);
