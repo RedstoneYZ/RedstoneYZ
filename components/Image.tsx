bc0e126aa85db7e75d386554ffabb3a4e773a133
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-04-19 19:41:43 +0800

    fix: warning about the image

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,5 +1,18 @@
 import NextImage, { ImageProps } from 'next/image'
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} height={0} />
+const Image = ({ ...rest }: ImageProps) => {
+  return (
+    <div style={{ width: rest.width, height: rest.height }}>
+      <NextImage
+        {...rest}
+        width="0"
+        height="0"
+        sizes="100vw"
+        style={{ width: '100%', height: 'auto' }}
+      />
+    </div>
+
+  );
+}
 
 export default Image
