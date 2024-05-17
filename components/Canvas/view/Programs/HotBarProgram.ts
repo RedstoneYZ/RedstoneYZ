6994275a9c5117db0615886cbf45c237f8259d92
yohe <j00968361@gmail.com>
2024-05-17 15:36:26 +0800

    feature: add hotbar selection

--- components/Canvas/view/Programs/HotBarProgram.ts
+++ components/Canvas/view/Programs/HotBarProgram.ts
@@ -3,7 +3,8 @@ import Program from "./Program";
 
 interface Uniforms {
   screensize: WebGLUniformLocation;
-  sampler: WebGLUniformLocation;
+  sampler1: WebGLUniformLocation;
+  sampler2: WebGLUniformLocation;
 }
 
 export default class HotBarProgram extends Program {
@@ -17,7 +18,7 @@ export default class HotBarProgram extends Program {
     super(parent, gl);
 
     this.program = this.createProgram();
-    this.uniform = this.setupUniform(["screensize", "sampler"]);
+    this.uniform = this.setupUniform(["screensize", "sampler1", "sampler2"]);
     this.abo = this.createAbo();
     this.vao = this.createVao();
 
@@ -36,7 +37,7 @@ export default class HotBarProgram extends Program {
 
 
     gl.bufferData(gl.ARRAY_BUFFER, this.getData(), gl.STATIC_DRAW);
-    gl.drawElements(gl.TRIANGLE_FAN, 4, gl.UNSIGNED_SHORT, 0);
+    gl.drawElements(gl.TRIANGLE_FAN, 9, gl.UNSIGNED_SHORT, 0);
     console.log(this.parent.controller.hotbarIndex)
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindVertexArray(null);
@@ -46,30 +47,51 @@ export default class HotBarProgram extends Program {
   }
 
   private getData(): Float32Array {
-    /*
-    const vs = Matrix4.Multiply(
-      new Float32Array([60, 20, 20, 1, 60, 20, -20, 1, 60, -20, -20, 1, 60, -20, 20, 1]),
-      Matrix4.RotateY((23.4 * Math.sin(phi) * Math.PI) / 180),
-      Matrix4.RotateZ(theta),
-      Matrix4.RotateX((25.04 * Math.PI) / 180),
-    );*/
+    
     const canvasWidth = this.parent.renderer.canvasW, canvasHeight = this.parent.renderer.canvasH;
-    const gui_pos = new Float32Array([0.23 * canvasWidth, 0.77 * canvasWidth,  (canvasHeight - 10) - 0.07 * canvasWidth, canvasHeight - 10]) // x1, x2, y1, y2
+    const edge = 0.06 * canvasWidth, x_offset = 0.23 * canvasWidth, y_offset = canvasHeight - 10;
+    const hotbarIndex = this.parent.controller.hotbarIndex;
+    const gui_pos = new Float32Array([x_offset, x_offset + 9 * edge,  y_offset - edge, y_offset]) // x1, x2, y1, y2
+    const sel_pos = new Float32Array([x_offset + (hotbarIndex - 0.05) * edge, x_offset + hotbarIndex * edge + 1.05 * edge,  y_offset - 1.05 * edge, y_offset + 0.05 * edge]) // x1, x2, y1, y2
     return new Float32Array([
       gui_pos[0],
       gui_pos[2],
       0,
       0,
+      0,
       gui_pos[0],
       gui_pos[3],
+      0,
       0,
       1,
       gui_pos[1],
       gui_pos[3],
+      0,
       1,
       1,
       gui_pos[1],
       gui_pos[2],
+      0,
+      1,
+      0,
+      sel_pos[0],
+      sel_pos[2],
+      1,
+      0,
+      0,
+      sel_pos[0],
+      sel_pos[3],
+      1,
+      0,
+      1,
+      sel_pos[1],
+      sel_pos[3],
+      1,
+      1,
+      1,
+      sel_pos[1],
+      sel_pos[2],
+      1,
       1,
       0,
     ]);
@@ -81,7 +103,8 @@ export default class HotBarProgram extends Program {
 
     gl.useProgram(this.program);
     gl.uniform2iv(uniform.screensize, [this.parent.renderer.canvasW, this.parent.renderer.canvasH]);
-    gl.uniform1i(uniform.sampler, 3);
+    gl.uniform1i(uniform.sampler1, 3);
+    gl.uniform1i(uniform.sampler2, 4);
     gl.useProgram(null);
 
     return uniform;
@@ -107,11 +130,14 @@ export default class HotBarProgram extends Program {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.abo);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0]), gl.STATIC_DRAW);
 
-    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 16, 0);
-    gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 16, 8);
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 20, 0);
+    gl.vertexAttribPointer(1, 1, gl.FLOAT, false, 20, 8);
+    gl.vertexAttribPointer(2, 2, gl.FLOAT, false, 20, 12);
+    
 
     gl.enableVertexAttribArray(0);
     gl.enableVertexAttribArray(1);
+    gl.enableVertexAttribArray(2);
 
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, gl.createBuffer());
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.parent.indices), gl.STATIC_DRAW);
@@ -133,7 +159,7 @@ export default class HotBarProgram extends Program {
       image.onload = () => res(image);
       image.src = "/images/textures/gui/hotbar.png";
     });
-
+    
     gl.activeTexture(gl.TEXTURE3);
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -141,6 +167,26 @@ export default class HotBarProgram extends Program {
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, hotbar);
+    
+    const hotbarsel = await new Promise<HTMLImageElement>((res) => {
+      const image = new Image();
+      image.onload = () => res(image);
+      image.src = "/images/textures/gui/hotbar_selection.png";
+    });
+
+    
+    const texture1 = gl.createTexture();
+    if (!texture1) {
+      throw new Error("Failed to create main texture.");
+    }
+
+    gl.activeTexture(gl.TEXTURE4);
+    gl.bindTexture(gl.TEXTURE_2D, texture1);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, hotbarsel);
 
     return texture;
   }
@@ -149,19 +195,23 @@ export default class HotBarProgram extends Program {
 
   protected vsSrc = `#version 300 es
     layout(location = 0) in vec2 a_position;
-    layout(location = 1) in vec2 a_texcoord;
+    layout(location = 1) in float gui_id;
+    layout(location = 2) in vec2 a_texcoord;
 
     uniform ivec2 screensize;
 
     out mediump vec2 v_texcoord;
+    out float v_gui_id;
 
     void main() {
 
       vec2 clipSpace = (a_position / vec2(screensize)) * 2. - 1.;
       
-
-      gl_Position = vec4(clipSpace * vec2(1., -1.), -0.999, 1);
+      if(gui_id == 0.) gl_Position = vec4(clipSpace * vec2(1., -1.), -0.999, 1);
+      else gl_Position = vec4(clipSpace * vec2(1., -1.), -0.9991, 1);
+      
       v_texcoord = a_texcoord;
+      v_gui_id = gui_id;
     }
   `;
 
@@ -169,13 +219,17 @@ export default class HotBarProgram extends Program {
     precision mediump float;
 
     in mediump vec2 v_texcoord;
+    in float v_gui_id;
 
-    uniform sampler2D sampler;
+    uniform sampler2D sampler1;
+    uniform sampler2D sampler2;
 
     out vec4 fragColor;
 
     void main() {
-      vec4 texel = texture(sampler, v_texcoord);
+      vec4 texel;
+      if(v_gui_id == 0.) texel = texture(sampler1, v_texcoord);
+      else texel = texture(sampler2, v_texcoord);
       if(texel.r == 0.) discard;
       fragColor = vec4(texel.rgb, 1.0);
     }
