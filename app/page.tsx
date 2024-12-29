6c02dc730ebabd84d40a286ccc643f9046c16c88
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-12-29 22:43:39 +0800

    chore: remove redundant Main.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,84 @@
-import Main from "./Main";
+import Link from "@/components/Link";
+import Tag from "@/components/Tag";
+import siteMetadata from "@/data/siteMetadata";
+import formatDate from "@/utils/formatDate";
+import getArticles from "@/utils/getArticles";
 
-export default async function Page() {
-  return <Main />;
+const MAX_DISPLAY = 3;
+const articles = getArticles();
+
+export default function Main() {
+  return (
+    <>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Homepage
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div>
+        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+          {!articles.length && "No articles found."}
+          {articles.slice(0, MAX_DISPLAY).map((post, i) => {
+            const { created, title, categories, link } = post;
+            return (
+              <li key={i} className="py-12">
+                <article>
+                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                    <dl>
+                      <dt className="sr-only">Published on</dt>
+                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                        <time dateTime={created}>{formatDate(created, siteMetadata.locale)}</time>
+                      </dd>
+                    </dl>
+                    <div className="space-y-5 xl:col-span-3">
+                      <div className="space-y-6">
+                        <div>
+                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                            <Link href={link} className="text-gray-900 dark:text-gray-100">
+                              {title}
+                            </Link>
+                          </h2>
+                          <div className="flex flex-wrap">
+                            {categories.map((cat) => (
+                              <Tag key={cat} text={cat} />
+                            ))}
+                          </div>
+                        </div>
+                        {/* TODO: <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                          {summary}
+                        </div> */}
+                      </div>
+                      <div className="text-base font-medium leading-6">
+                        <Link
+                          href={link}
+                          className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                          aria-label={`Read more: "${title}"`}
+                        >
+                          Read more &rarr;
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </article>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {articles.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/article"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="All posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+    </>
+  );
 }
