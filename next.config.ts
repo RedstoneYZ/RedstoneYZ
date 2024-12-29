93ea9062021367fa18ff7c37e1939f384d58e707
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-12-29 12:26:19 +0800

    refactor: metadata format

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-import createMDX from '@next/mdx'
+import createMDX from '@next/mdx';
+import rehypeKatex from 'rehype-katex';
+import rehypePrismPlus from 'rehype-prism-plus';
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include markdown and MDX files
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-  // Optionally, add any other Next.js config below
 }
  
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
+  options: {
+    rehypePlugins: [rehypeKatex, rehypePrismPlus]
+  }
 })
  
 // Merge MDX config with Next.js config
---
+++ scripts/generateMetadata.mjs
@@ -0,0 +1,38 @@
+import fs from "fs";
+
+const PATH = "./app/article";
+
+fs.writeFileSync("./data/articleMetadata.json", JSON.stringify(readData(PATH, ""), null, 2));
+
+function readData(fullPath, path) {
+  if (path.endsWith(".mdx")) {
+    const content = fs.readFileSync(fullPath).toString();
+    return parseMetadata(content);
+    throw new Error(`${fullPath} does not have a title`);
+  }
+
+  if (fs.statSync(fullPath).isDirectory()) {
+    const children = fs.readdirSync(fullPath);
+    const result = {};
+    children.forEach((child) => {
+      const content = readData(fullPath + "/" + child, child);
+      if (!content) return;
+      result[child === "page.mdx" ? "page" : child] = content;
+    });
+    return result;
+  }
+}
+
+function parseMetadata(content) {
+  const match = content.match(/^\{\/\*([\s\S]+?)\*\/\}/);
+  if (!match || !match[1]) {
+    return {};
+  }
+
+  return Object.fromEntries(
+    match[1]
+      .trim()
+      .split("\n")
+      .map((a) => a.split(/ *\: */)),
+  );
+}
