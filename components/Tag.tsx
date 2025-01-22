8a139d2729301a74b9c48c5fcbe0159d79deca06
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2025-01-22 16:09:32 +0800

    chore: put categories to todo list

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link";
+// import Link from "next/link";
+
 interface Props {
   text: string;
 }
 
 const Tag = ({ text }: Props) => {
   return (
-    <Link
-      href={`/categories/${text}`}
-      className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+    <span
+      // href={`/categories/${text}`}
+      className="mr-3 text-sm font-medium uppercase"
     >
       {text.split(" ").join("-")}
-    </Link>
+    </span>
   );
 };
 
