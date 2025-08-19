4457ac66b963a9a84c9754159c5f1f44d3bd2474
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2025-08-19 18:42:09 +0800

    lint

--- components/article/Inventory.tsx
+++ components/article/Inventory.tsx
@@ -12,28 +12,33 @@ type InventoryItem = {
 
 export default function Inventory({ items }: { items: InventoryItem[] }) {
   return (
-    <div style={{
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      margin: "20px 0"
-    }}
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        margin: "20px 0",
+      }}
     >
-      <div style={{
-        width: 352,
-        height: 136 + 108 * ~~(items.length / 27),
-        padding: 14,
-        display: "grid",
-        gridTemplateColumns: "36px 36px 36px 36px 36px 36px 36px 36px 36px",
-        gridTemplateRows: "36px 36px 36px",
-        backgroundSize: "cover",
-        imageRendering: "pixelated",
-        backgroundImage: `url(/images/inventory/inventory_${~~(items.length / 27 + 1) * 3}.png)`
-      }}>
-        {items.map((item, i) => <Item key={i} item={item} />)}
+      <div
+        style={{
+          width: 352,
+          height: 136 + 108 * ~~(items.length / 27),
+          padding: 14,
+          display: "grid",
+          gridTemplateColumns: "36px 36px 36px 36px 36px 36px 36px 36px 36px",
+          gridTemplateRows: "36px 36px 36px",
+          backgroundSize: "cover",
+          imageRendering: "pixelated",
+          backgroundImage: `url(/images/inventory/inventory_${~~(items.length / 27 + 1) * 3}.png)`,
+        }}
+      >
+        {items.map((item, i) => (
+          <Item key={i} item={item} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
 function Item({ item: { id, count = 1 } }: { item: InventoryItem }) {
@@ -48,51 +53,78 @@ function Item({ item: { id, count = 1 } }: { item: InventoryItem }) {
     ({ icon, chinese, members } = blocks);
   }
 
-  const displayText = <span>
-    {id.startsWith("#") ? "任何" : ""}{chinese}
-    {members.length ? <><br/><br/>{id.startsWith("#") ? "如" : ""}{members.map(m => translation[m as keyof typeof translation] ?? m).join("、")}</> : ""}
-  </span>
+  const displayText = (
+    <span>
+      {id.startsWith("#") ? "任何" : ""}
+      {chinese}
+      {members.length ? (
+        <>
+          <br />
+          <br />
+          {id.startsWith("#") ? "如" : ""}
+          {members.map((m) => translation[m as keyof typeof translation] ?? m).join("、")}
+        </>
+      ) : (
+        ""
+      )}
+    </span>
+  );
 
-  return <>
-    <div style={{
-      width: 36,
-      height: 36,
-      padding: 2,
-    }} onMouseMove={(e) => {
-      setTooltip(() => ({ x: e.clientX + 10, y: e.clientY - 40, visible: true }));
-    }} onMouseLeave={() => {
-      setTooltip(a => ({ ...a, visible: false }));
-    }}>
-      <div style={{
-        position: "relative",
-        width: 32,
-        height: 32,
-        backgroundSize: "cover",
-        imageRendering: "pixelated",
-        backgroundImage: `url(/images/inventory/${icon}.png)`
-      }}>
-        {count === 1 ? <></> : <span style={{
-          position: "absolute",
-          top: 19,
-          right: -2,
-          userSelect: "none",
-          fontFamily: "Minecraftia",
-          fontSize: 16,
-          color: "white",
-          textShadow: "2px 2px 0px #3F3F3F"
-        }}>{count}</span>}
+  return (
+    <>
+      <div
+        style={{
+          width: 36,
+          height: 36,
+          padding: 2,
+        }}
+        onMouseMove={(e) => {
+          setTooltip(() => ({ x: e.clientX + 10, y: e.clientY - 40, visible: true }));
+        }}
+        onMouseLeave={() => {
+          setTooltip((a) => ({ ...a, visible: false }));
+        }}
+      >
+        <div
+          style={{
+            position: "relative",
+            width: 32,
+            height: 32,
+            backgroundSize: "cover",
+            imageRendering: "pixelated",
+            backgroundImage: `url(/images/inventory/${icon}.png)`,
+          }}
+        >
+          {count === 1 ? (
+            <></>
+          ) : (
+            <span
+              style={{
+                position: "absolute",
+                top: 19,
+                right: -2,
+                userSelect: "none",
+                fontFamily: "Minecraftia",
+                fontSize: 16,
+                color: "white",
+                textShadow: "2px 2px 0px #3F3F3F",
+              }}
+            >
+              {count}
+            </span>
+          )}
+        </div>
       </div>
-    </div>
-    {
-      tooltip.visible && <div
+      {tooltip.visible && (
+        <div
           style={{
             position: "fixed",
             top: tooltip.y,
             left: tooltip.x,
-            maxWidth: 352, 
+            maxWidth: 352,
             color: "white",
             padding: "6px 8px",
-            border: "4px solid black", 
+            border: "4px solid black",
             borderImage: "url(/images/inventory/tooltip.png) 4 fill repeat",
             pointerEvents: "none", // don't block mouse
             zIndex: 10,
@@ -100,11 +132,12 @@ function Item({ item: { id, count = 1 } }: { item: InventoryItem }) {
         >
           {displayText}
         </div>
-    }
-  </>
+      )}
+    </>
+  );
 }
 
-function parseTags(id: string): { icon: string, chinese: string, members: string[] } {
+function parseTags(id: string): { icon: string; chinese: string; members: string[] } {
   const list = tag_table[id as keyof typeof tag_table];
   if (!list) {
     throw new Error(`${id} is not a registered tag`);
@@ -114,8 +147,7 @@ function parseTags(id: string): { icon: string, chinese: string, members: string
   for (const item of list.members) {
     if (item.startsWith("#")) {
       result.push(...parseTags(item.substring(1)).members);
-    }
-    else {
+    } else {
       result.push(item);
     }
   }
--- components/article/Table.tsx
+++ components/article/Table.tsx
@@ -1,27 +1,37 @@
 interface TableData {
   header: string[];
   body: string[][];
-};
+}
 
 export default function Table({ data }: { data: TableData }) {
   return (
     <table className="my-4 w-full px-5">
       <thead>
         <tr className="rounded-lg bg-primary-300 dark:bg-primary-600">
-          {
-            data.header.map((h, i) => <td key={i} className={`p-3 text-center ${i === 0 ? "rounded-tl-lg" : i === data.header.length - 1 ? "rounded-tr-lg" : ""}`}>{h}</td>)
-          }
+          {data.header.map((h, i) => (
+            <td
+              key={i}
+              className={`p-3 text-center ${i === 0 ? "rounded-tl-lg" : i === data.header.length - 1 ? "rounded-tr-lg" : ""}`}
+            >
+              {h}
+            </td>
+          ))}
         </tr>
       </thead>
       <tbody>
-        {
-          data.body.map((r, i) => 
-            <tr key={i} className={i & 1 ? "bg-gray-200 dark:bg-gray-900" : "bg-gray-100 dark:bg-gray-800"}>{
-              r.map((c, j) => <td key={j} className="p-3 text-center">{c}</td>)
-            }</tr>)
-        }
+        {data.body.map((r, i) => (
+          <tr
+            key={i}
+            className={i & 1 ? "bg-gray-200 dark:bg-gray-900" : "bg-gray-100 dark:bg-gray-800"}
+          >
+            {r.map((c, j) => (
+              <td key={j} className="p-3 text-center">
+                {c}
+              </td>
+            ))}
+          </tr>
+        ))}
       </tbody>
-
     </table>
   );
 }
